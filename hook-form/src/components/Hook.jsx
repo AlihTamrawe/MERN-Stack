0f4a829c511b1efc63cbe2ca6b1a9bc531ca3b13
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2022-12-08 15:33:27 +0200

    commit

--- hook-form/src/components/Hook.jsx
+++ hook-form/src/components/Hook.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from  'react';
 
+
 const  Hook = (props) =>{
     const [firstname ,setfirstname] = useState("");
     const [firstnameError ,setfirstnameError] = useState("");
@@ -20,6 +21,7 @@ const  Hook = (props) =>{
     const [confirms ,setconfirm] = useState("");
     const [confirmsError ,setconfirmsError] = useState("");
 
+    var flag = false;
 
 
     const createUser = (e) => {
@@ -27,37 +29,71 @@ const  Hook = (props) =>{
         const newUser = { firstname,lastname, email, password,confirmpassword };
                 console.log("Welcome", newUser);
                 
+
     };
+    const handleconfirm =(e)=> {
+        if(firstnameError==""&&lastnameError==""&&emailError==""&&passwordError==""&&confirmpasswordError==""){
+            setconfirmsError("Ready submission");
+
+        }
+        else
+        {
+            setconfirmsError(" Not Ready submission");
+
+        }
+    }
     
 
     const handlefirstname = (e) => {
         setfirstname(e.target.value);
         if(e.target.value.length < 1) {
             setfirstnameError("firstname is required!");
+            flag=false;
+
         } else if(e.target.value.length < 2) {
             setfirstnameError("firstname must be 2 characters or longer!");
+                        flag=false;
         } else {
             setfirstnameError("");
+            flag=true;
         }
     }
     const handlelastname = (e) => {
         setlastname(e.target.value);
         if(e.target.value.length < 1) {
             setlastnameError("lasttname is required!");
+            flag=false;
+
         } else if(e.target.value.length < 4) {
             setlastnameError("lasttname must be 4 characters or longer!");
+            flag=false;
+
         } else {
             setlastnameError("");
+            flag=true;
+
         }
     }
     const handleEmail= (e) => {
         setEmail(e.target.value);
+        let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regEmail.test( e.target.value) ) {
+            setemailError( "Please enter a valid email address");
+            flag=false;
+
+        }else
         if(e.target.value.length < 1) {
             setemailError("email is required!");
+            flag=false;
+
         } else if(e.target.value.length < 7) {
             setemailError("email must be 7 characters or longer!");
+            flag=false;
+
         } else {
             setemailError("");
+            flag=true;
+
         }
     }
 
@@ -65,35 +101,52 @@ const  Hook = (props) =>{
         setpassword(e.target.value);
         if(e.target.value.length < 1) {
             setpasswordError("password is required!");
+            flag=false;
+
         } else if(e.target.value.length < 8) {
             setpasswordError("password must be 8 characters or longer!");
+            flag=false;
+
         } else {
             setpasswordError("");
+            flag=true;
+
         }
     }
 
     const handleconpassword= (e) => {
         setConfirmpassword(e.target.value);
+        
         if(e.target.value.length < 1) {
             setconfirmpasswordError("password is required!");
+            flag=false;
+
         } else if(e.target.value!=password) {
             setconfirmpasswordError("Confirm your password as Same");
+            flag=false;
+
         }else if(e.target.value.length < 8){
             setconfirmpasswordError("confirm must be 8 characters or longer!");
+            flag=false;
 
         } 
-         else {
+        else {
             setconfirmpasswordError("");
+            flag=true;
         }
     }
 
 
     return (
         <div>
+             
+                    <p style={{color:'red'}}>{ confirmsError }</p> :
+                  
+
             <form style={{border:"solid 2px gray"}} onSubmit={ createUser }>
                 <div  >
                     <label>Firstname: </label> 
-                    <input type="text" onChange={ (e,handlefirstname) => setfirstname(e.target.value)  } onKeyDown={handlefirstname} value={ firstname } />
+                    <input type="text" onChange={ handlefirstname } value={ firstname } />
                     {
                     firstnameError ?
                     <p style={{color:'red'}}>{ firstnameError }</p> :
@@ -102,7 +155,7 @@ const  Hook = (props) =>{
                 </div>
                 <div >
                     <label>Lastname: </label> 
-                    <input type="text" onChange={ (e) => setlastname(e.target.value) }  onKeyDown={handlelastname} value={ lastname } />
+                    <input type="text" onChange={ handlelastname }  value={ lastname } />
                     {
                     lastnameError ?
                     <p style={{color:'red'}}>{ lastnameError }</p> :
@@ -111,7 +164,7 @@ const  Hook = (props) =>{
                 </div>
                 <div >
                     <label>Email Address: </label> 
-                    <input type="text" onChange={ (e) => setEmail(e.target.value) } onKeyDown={handleEmail} value={email} />
+                    <input type="text" onChange={handleEmail }  value={email} />
                     {
                     emailError ?
                     <p style={{color:'red'}}>{ emailError }</p> :
@@ -120,7 +173,7 @@ const  Hook = (props) =>{
                 </div >
                 <div >
                     <label>Password: </label>
-                    <input type="password" onChange={ (e) => setpassword(e.target.value) }  onKeyDown={handlepassword} value={password} />
+                    <input type="password" onChange={ handlepassword }   value={password} />
                     {
                     passwordError ?
                     <p style={{color:'red'}}>{ passwordError }</p> :
@@ -136,7 +189,7 @@ const  Hook = (props) =>{
                     ''
                 }
                 </div>
-                <input type="submit" value="Create User" />
+                <input type="submit" value="Create User" onClick={handleconfirm} />
             </form>
 
             <div >
