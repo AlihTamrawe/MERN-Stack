1c0ca4a1fb7e4b582405028d75d8840c9455f583
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2022-12-14 08:38:26 +0200

    commit

--- api-es/src/App.js
+++ api-es/src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
 import React from 'react'
 import ApiPok from './components/ApiPok';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
+const Home = (props) => { 
+  return (
+    <h1 style={{color: "red"}}>Home Component</h1>
+  );
+}
+    
+const About = (props) => {
+  return (
+    <h1 style={{color: "blue"}}>About Component</h1>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <ApiPok/>
+      <h1>Welcome </h1>
+
+     <p> <Link to={"/about"}>Go to About </Link></p> 
+      <p><Link to={"/Api"}>Go to Api </Link></p>
+      <p><Link to={"/"}>Go to Home </Link></p>
+
+
+      <Routes>
+            <Route path="/about" element={<About />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/Api" element={<ApiPok />} />
+
+
+
+      </Routes> 
+      
     </div>
   );
 }
--- api-es/src/components/ApiPok.jsx
+++ api-es/src/components/ApiPok.jsx
@@ -1,30 +1,123 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
-const  ApiPok =()=> {
-    const [ourres,setOurres]=useState([]);
-    const [loaded,setLoaded]=useState(false
-        );
+const  ApiPok =(props)=> {
+  
+    const [ourresstarships,setOurresstarships]=useState([]);
+    const [ourrespeople,setOurrespeople]=useState([]);
+    const [ourresplantes,setOurresplantes]=useState([]);
+
+    const [loaded,setLoaded]=useState(false);
+    const [id, setid] = useState(0)
+    const [person, setperson] = useState(0)
+    const [kind, setkind] = useState("people")
+
+
+
+
+
 
 
 
     useEffect(()=>{
-        axios.get("https://pokeapi.co/api/v2/pokemon")
-        .then(pok =>{console.log(pok); setOurres(pok.data.results);setLoaded(true)})
+        // axios.get("https://pokeapi.co/api/v2/pokemon")
+        axios.get("https://swapi.dev/api/people")
+        .then(pok =>{console.log(pok); setOurrespeople(pok.data.results);setLoaded(true)})
         .catch(err=>console.log(err))
+        axios.get("https://swapi.dev/api/starships")
+        .then(pok =>{console.log(pok); setOurresstarships(pok.data.results);setLoaded(true)})
+        .catch(err=>console.log(err))
+        axios.get("https://swapi.dev/api/planets")
+        .then(pok =>{console.log(pok); setOurresplantes(pok.data.results);setLoaded(true)})
+        .catch(err=>console.log(err))
+
+        
     },[])
+  
+
+    const handelSearch =(e)=>{
+      if(kind=="planets"){
+        const num = ourresplantes[id];
+        console.log(num.name)
+        setperson(num)
+
+      }
+      if(kind=="people"){
+        const num = ourrespeople[id];
+        console.log(num.name)
+        setperson(num)
+
+      }
+      if(kind=="starships"){
+        const num = ourresstarships[id];
+        console.log(num.name)
+        setperson(num)
+        
+
+      }
+     
+      
+
+    }
+
 
   return (
 
 
     <div>
         <h4>Fetch Pokemon</h4>
-                <ul>
+           
 
-        {loaded && ourres.map( pok => 
-        <li><p>{pok.name}</p></li>
-        )}
+                <select onChange={e=>setkind(e.target.value) } >
+       
+        
+        <option value={"starships"}>starships</option>
+        <option value={"planets"}>planets</option>
+        <option value={"people"}>people</option>
+
+        
+             </select>
+             <input type="number"  onChange={e=>setid(e.target.value)} />
+             <button onClick={handelSearch}>Search</button>
+
+              { kind=="people" ? 
+
+             
+             <ul>
+             
+                <h1> Name :{person.name}</h1>
+                <p>Height : {person.height}</p>
+                <p>Mass : {person.mass}</p>
+                <p>Hair color : {person.hair_color}</p>
+                <p>Skin color : {person.skin_color}</p>
+
+                
+
+                
+             </ul>
+             :kind=="planets"? 
+              <ul>
+              <h1> Name :{person.name}</h1>
+
+              <p>climate : {person.climate}</p>
+              <p>terrain : {person.terrain}</p>
+              <p>diameter : {person.diameter}</p>
+
+              
               </ul>
+               :
+               <ul>
+             
+               <h1> Name :{person.name}</h1>
+               <p>length : {person.model}</p>
+               <p>manufacturer : {person.manufacturer}</p>
+               <p>passengers : {person.passengers}</p>
+               <p>consumables : {person.consumables}</p>
+
+               
+               
+            </ul>
+              }
 
     </div>
   )
