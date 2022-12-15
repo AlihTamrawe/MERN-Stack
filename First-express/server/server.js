bfcc9f2b43bc484cbe5330f6bc7511a792964b0f
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2022-12-15 13:22:22 +0200

    commit

--- First-express/server/server.js
+++ First-express/server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const { faker } = require('@faker-js/faker');
+const { json } = require("express");
 
 const users = [
   { firstName: "Reimu",  lastName: "Hakurei"    },
@@ -8,14 +10,47 @@ const users = [
   { firstName: "Sakuya", lastName: "Izayoi"     },
   { firstName: "Momiji", lastName: "Inubashiri" }
 ];
+
+
+const createcompany =() => {
+  const  company={
+  name:faker.company.name(),
+  address:{
+    street:faker.address.street(),
+    city:faker.address.cityName(),
+    state:faker.address.state(),
+    zipCode:faker.address.zipCode(),
+    country:faker.address.country()
+  },
+  email:faker.internet.email(),
+  _id:faker.datatype.uuid()
+}
+return company;
+}
+
+const createuser =() => {
+  const  user={
+  firstName:faker.name.firstName(),
+  lastName:faker.name.lastName(),
+  email:faker.internet.email(),
+  phone:faker.phone.number(),
+  passowrd:faker.internet.password(),
+  _id:faker.datatype.uuid()
+}
+return user;
+}
 // req is short for request
 // res is short for response
 app.get("/api", (req, res) => {
+  res.json(createuser())
+
   res.send("Our express api server is now sending this over to the browser");
+
 });
 
 const server = app.listen(8000, () =>
   console.log(`Server is locked and loaded on port ${server.address().port}!`)
+  
 );
 console.log("no way")
 
@@ -36,3 +71,20 @@ app.get("/api/users/:id", (req, res) => {
   // assuming this id is the index of the users array we could return one user this way
   res.json( users[req.params.id] );
 });
+const createProduct = () => {
+  const newFake = {
+      name: faker.commerce.productName(),
+      price: "$" + faker.commerce.price(),
+      department: faker.commerce.department()
+  };
+  return newFake;
+};
+  
+const newFakeProduct = createProduct();
+console.log(newFakeProduct);
+
+const newuser = createuser();
+console.log(newuser);
+
+const newcompany = createcompany();
+console.log(newcompany);
